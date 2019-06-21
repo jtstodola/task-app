8868d0db00124d945b30ee55418359ae63db153e
Jessica <jtstodola@gmail.com>
2019-06-20 21:11:34 -0500

    indentation

--- src/views/Tasks/TomorrowTasks.js
+++ src/views/Tasks/TomorrowTasks.js
@@ -23,20 +23,20 @@ const getDateTomorrow = () => {
   }
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      justifyContent: 'space-around',
-      overflow: 'hidden',
-      backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-      width: 500,
-      height: 450,
-    },
-    icon: {
-      color: 'rgba(255, 255, 255, 0.54)',
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
 }));
 
 
